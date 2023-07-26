0fa1ffe9afedca9fd6aafb4e1aabd2c571e5c7f9
Jacob Jeffries <jjeffrie@nmu.edu>
2023-07-26 15:09:09 -0400

    Completed Lesson 24

---
+++ 24l_Rewriting/Database.php
@@ -0,0 +1,46 @@
+<?php
+
+  class Database {
+
+    public $connection;
+    public $statement;
+
+    public function __construct($config, $username='root', $password='password')
+    {
+
+      $dsn = 'mysql:'.http_build_query($config, '', ';'); // More Simple way to string together the dsn variable
+
+      // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};user=root;password=password;charset={$config['charset']}";
+
+      $this->connection = new PDO($dsn, $username, $password, [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
+    }
+
+    public function query($query, $params=[]){
+      $this->statement = $this->connection->prepare($query);
+      $this->statement->execute($params);
+      return $this;     
+      
+      // FetchAll return a list even if the response only has one item
+      // $posts = $statement->fetch(PDO::FETCH_ASSOC);
+      // return $posts;
+    }
+
+    public function find(){
+      return $this->statement->fetch();
+    }
+
+    public function get(){
+      return $this->statement->fetchAll();
+    }
+    
+    public function findOrFail(){
+      $result = $this->find();
+      if(!$result){
+        abort(Response::NOT_FOUND);
+      }
+      return $result;
+    }
+  }
+?>
---
+++ 24l_Rewriting/view/note.view.php
@@ -0,0 +1,18 @@
+<?php require 'view/partials/head.php'; ?>
+<?php require 'view/partials/nav.php'; ?>
+<?php require 'view/partials/banner.php'; ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <p class="font-bold underline">Note: <?= $note['id'] ?></p>
+      <br />
+      <?php 
+        echo "<p>{$note['body']}</p>";
+      ?>
+      <br />
+      <a href="/notes?user_id=<?= $note['user_id'] ?>" class="text-blue-500 hover:underline">Got Back to Full Notes List</a>
+    </div>
+    <?php require 'view/partials/info.php'; ?>
+  </main>
+
+<?php require 'view/partials/footer.php'; ?>
