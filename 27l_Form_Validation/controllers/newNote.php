a2d9f9a5b8c96c3b489d54c39f5bdf5b478428c3
Jacob Jeffries <jjeffrie@nmu.edu>
2023-07-27 10:24:55 -0400

    Completed Lesson 27

--- 27l_Form_Validation/controllers/newNote.php
+++ 27l_Form_Validation/controllers/newNote.php
@@ -4,16 +4,47 @@
   $config = require "config.php";
   $db = new Database($config['database']);
 
-  if($_SERVER['REQUEST_METHOD'] === "POST"){
+  if ($_SERVER['REQUEST_METHOD'] === "POST"){
     // dd('You submitted a form.');
     // dd($_POST);
 
-    $body = addslashes(trim(strip_tags($_POST['body'])));
+    // if(strlen($_POST['body'] < 1000 && strlen($_POST['body']) > 0)){
 
-    $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
-      'body' => $body,
-      'user_id' => 1 //$_POST['user_id']
-    ]);
+    //    $body = addslashes(trim(strip_tags($_POST['body'])));
+
+    //   $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
+    //     'body' => $body,
+    //     'user_id' => 1 //$_POST['user_id']
+    //   ]);
+    // } else {
+    //   echo "Nope - the body of a note must be between 1 - 1000 characters. I bet you thought that curl could bypass my kung-fu?\n\n";
+    //   die();
+    // }
+
+    $errors = [];
+
+    // dd(strlen($_POST['body']));
+
+    if (strlen($_POST['body']) === 0) {
+      $errors['body'] = "The Body of a note cannot be empty.";
+      // dd($errors);
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+      $errors['body'] = "The Body of a note cannot exceed 1000 Characters.";
+    }
+
+    if (empty($errors)) {
+      $body = addslashes(trim(strip_tags($_POST['body'])));
+
+      // dd($errors);
+
+      $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
+        'body' => $body,
+        'user_id' => 1 //$_POST['user_id']
+      ]);
+      header("Location: /notes");
+    } 
   }
 
   require 'view/newNote.view.php';
