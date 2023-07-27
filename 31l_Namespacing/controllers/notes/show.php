0b6cf24d363ab5bd25b2f85ac8391c42f907c614
Jacob Jeffries <jjeffrie@nmu.edu>
2023-07-27 14:53:20 -0400

    31

---
+++ 31l_Namespacing/controllers/notes/show.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = require base_path('config.php');
+$db = new Core\Database($config['database']);
+
+$currentUserId = 1;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
+
+// dd($note);
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
---
+++ 31l_Namespacing/views/index.view.php
@@ -0,0 +1,12 @@
+<?php view('/partials/head.php'); ?>
+<?php view('/partials/nav.php'); ?>
+<?php view('/partials/banner.php'); ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      Home Page
+    </div>
+    <?php view('/partials/info.php'); ?>
+  </main>
+
+<?php view('/partials/footer.php'); ?>
